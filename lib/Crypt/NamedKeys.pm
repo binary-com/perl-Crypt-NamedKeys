ae2a6b85f9b2b93c88ad3b8906cb0c661a9f8739
Chris Travers <christravers@binary.com>
2014-10-30 03:52:54 +0000

    first version of code reviewed and rewritten.

--- lib/Crypt/NamedKeys.pm
+++ lib/Crypt/NamedKeys.pm
@@ -1,136 +1,364 @@
 package Crypt::NamedKeys;
 
-use 5.006;
-use strict;
-use warnings FATAL => 'all';
+use Moo;
 
 =head1 NAME
 
-Crypt::NamedKeys - The great new Crypt::NamedKeys!
+Crypt::NamedKeys
 
-=head1 VERSION
-
-Version 0.01
-
-=cut
-
-our $VERSION = '0.01';
-
-
-=head1 SYNOPSIS
-
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
+=head1 SYNOPSYS
 
     use Crypt::NamedKeys;
+    my $crypt = Crypt::NamedKeys->new(keyname => 'href');
+    my $encrypted = $crypt->encrypt_data(data => $href);
+    my $restored_href = $crypt->decrypt_data(
+        data => $encrypted->{data},
+        mac  => $encrypted->{mac},
+    );
 
-    my $foo = Crypt::NamedKeys->new();
-    ...
+=head1 DESCRIPTION
 
-=head1 EXPORT
+This module provides functions to serialize data for transfer via non-protected
+channels with encryption and data integrity protection.  The module tracks key
+number used to encrypt information so that keys can be rotated without making
+data unreadable.
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+=head1 CONFIGURATION AND KEY ROTATION
 
-=head1 SUBROUTINES/METHODS
+The keys are stored in the keyfile, configurable as below.  Keys are numbered 
+starting at 1.  Numbers must never be reused.  Typically key rotation will be 
+done in several steps, each with its own rollout.  These steps MUST be done as 
+separate releases because otherwise keys may not be available to decrypt data, 
+and so things may not work.
 
-=head2 function1
+=head2 keyfile location
 
-=cut
+The keyfile can be set using the keyfile($path) function.  There is no default.
 
-sub function1 {
-}
+=head2 keyfile format
 
-=head2 function2
+The format of the keyfile is YAML, following a basic structure of
 
-=cut
+ keyname:
+    [keyhashdef]
 
-sub function2 {
-}
+so for example:
 
-=head1 AUTHOR
+ cryptedfeed:
+    default_keynum: 9
+    none: queith7eeTh0teejaichoodobooX9ceechee9Sai9gauChiengaeraew3aDiehei
+    1: aePh8ahBaNg1bee6ohj3er5cuzeepoophai1oogohpoixothah4AuYiongu4ahta
+    2: oht1eep8uxoo1eeshaSaemee9aem5chahqueu0Aedaa7eeXae9aeghe5umoNah6a
+    3: chigh4veifoofe0Vohphee4ohkaef9giz2iaje2ahF4ohboSh6ifaiNgohwohchi
+    4: Ahphahmisaingo5Ietheangeegi5ia1uuF9taerooShaitoh1Eophig3ohziejet
+    5: oe5wi2equee6FeiZohjah2peas6Ahquohniefeimai0beip2waxeizoo1OhthohN
+    6: eigaezee3CeuC8phae4giph6Miqu6piy3Eideipahticesheij7se9eecai9fiez
+    7: DuuGhohViGh0Sheihahr6ce4Phuin7ahpaiSa5jaiphie3eiz8oa3dohrohghuow
+    8: ahfoniemah4boemeN8seJ7hohhualeetei7aegohhai5ohwahlohnah2Ee2Ewal1
+    9: Ceixei4shelohxee1ohdoochuliebael1kae8eit0Geeth1so9fohZi0cohs8go4
+    10: boreiDe0shueNgie7shai7ooc1yaeveiKeihuox0xahp1hai8phe7aephiel2oob
 
-Binary.com, C<< <perl at binary.com> >>
+In general we assume key spefications to use numeric keys within the named 
+key hash.  This makes key rotation a lot easier and prevents reusing key 
+numbers.
 
-=head1 BUGS
+All keys listed can be used for decryption (with the special 'none' key used if
+no key number is specified in the cyphertex), but by default only the default 
+keynumber (default_keynum, in this case 9) is used for encrypting.
 
-Please report any bugs or feature requests to C<bug-crypt-namedkeys at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Crypt-NamedKeys>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+The keynumber is specified in the resulting cyphertext so we know which key 
+to use for decrypting the cyphertext even if we don't try to decrypt it.  This
+allows:
 
+=over
 
+=item Key checking
 
+If you store cyphertext in your rdbms, you can check which keys are used before 
+you remove decryption support for a key.
 
-=head1 SUPPORT
+=item Orderly key rotation
 
-You can find documentation for this module with the perldoc command.
-
-    perldoc Crypt::NamedKeys
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker (report bugs here)
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Crypt-NamedKeys>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/Crypt-NamedKeys>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/Crypt-NamedKeys>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/Crypt-NamedKeys/>
+You can add a key, and later depricate it, managing the transition (and perhaps
+even using logging to know when the old key is no longer needed).
 
 =back
 
+=head2 Step 1:  Adding a New Key
 
-=head1 ACKNOWLEDGEMENTS
+In many cases you need to be able to add and remove keys without requiring that
+everything gets the new keys at the same time.  For example if you have multiple
+production systems, they are likely to get updated in series, and if you expect
+that everyone gets the keys at the same time, timing issues may occur.
 
+For this reason, we recommend breaking up the encryption key rollout into a
+number of steps.  The first one is making sure that everyone can use the
+new key to decrypt before anyone uses it to encrypt.
 
-=head1 LICENSE AND COPYRIGHT
+The first release is by adding a new key so that it is available for decryption.
 
-Copyright 2014 Binary.com.
+For example, in the keyfile suppose one has:
 
-This program is distributed under the (Revised) BSD License:
-L<http://www.opensource.org/licenses/BSD-3-Clause>
+  mykey:
+    default_keynum: 1
+    none: rsdfagtiaueIUPOIUYHH
+    1: rsdfagtiaueIUPOIUYHH
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions
-are met:
+We might add another line
 
-* Redistributions of source code must retain the above copyright
-notice, this list of conditions and the following disclaimer.
+    2: IRvswqerituq-HPIOHJHGdeewrwyugfrGRSe3eyy6te
 
-* Redistributions in binary form must reproduce the above copyright
-notice, this list of conditions and the following disclaimer in the
-documentation and/or other materials provided with the distribution.
+Once this file is released, the key number 2 will be available globally for
+decryption purposes, but everything will still be encrypted using key number 1.
 
-* Neither the name of Binary.com's Organization
-nor the names of its contributors may be used to endorse or promote
-products derived from this software without specific prior written
-permission.
+This means it is safe then to go onto the second step.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+=head2 Step 2:  Setting the new key as default
 
+Once the new keys have been released, the next step is to change the default 
+keynumber.  Data encrypted in this way will be available even to servers waiting
+to be updated because the keys have previously been rolled out.  To do this,
+simply change the default_keynum:
+
+  mykey:
+    default_keynum: 1
+    1: rsdfagtiaueIUPOIUYHH
+    2: IRvswqerituq-HPIOHJHGdeewrwyugfrGRSe3eyy6te
+
+becomes:
+
+  mykey:
+    default_keynum: 2
+    1: rsdfagtiaueIUPOIUYHH
+    2: IRvswqerituq-HPIOHJHGdeewrwyugfrGRSe3eyy6te
+
+Now all new data will be encrypted using keynumber 2.
+
+=head2 Step 3:  Retiring the old key
+
+Once the old key is no longer being used, it can be retired by deleting the 
+row.
+
+=head2 The Special 'none' keynum
+
+For aes keys before the key versioning was introduced, there is no keynum 
+associated with the cyphertext, so we use this key.
 
 =cut
 
-1; # End of Crypt::NamedKeys
+use Carp;
+use Crypt::CBC;
+use Digest::SHA qw(hmac_sha256_base64 sha256);
+use JSON;
+use MIME::Base64;
+use String::Compare::ConstantTime;
+use Try::Tiny;
+use YAML::XS;
+
+our $VERSION = '0.0.1';
+
+has keynum => (
+    is      => 'ro',
+    isa     => 'Maybe[Str]',
+    lazy    => 1,
+    builder => '_default_keynum',
+);
+
+has keyname => (
+    is       => 'ro',
+    isa      => 'Str',
+    required => 1,
+);
+
+my $keyfile;
+sub keyfile {
+    my $file = shift;
+    $file = shift if $file eq __PACKAGE__;
+    return $keyfile unless $file;
+    $keyfile = $file;
+}
+
+my $keyhash;
+
+my $get_keyhash = sub {
+   return $keyhash if $keyhash;
+   reload_keyhash();
+   return $keyhash;
+}
+
+sub reload_keyhash {
+    croak 'No keyfile defined (use keyfile() to set)' unless $keyfile;
+    $keyhash = YAML::XS::LoadFile($keyfile);
+    return scalar keys %$keyhash;
+}
+
+my $get_secret = sub {
+    my %args = @_;
+    croak 'No key name specified'   unless $args{keyname};
+    croak 'No key number specified' unless $args{keynum};
+    my $keytab = $get_keyhash()->{$args{keyname}};
+    return $keytab->{$args{keynum}};
+};
+
+sub _default_keynum {
+    my $self   = shift;
+    my $keytab = $get_keyhash()->{$self->keyname};
+    warn 'No default key found for ' . $self->keyname
+      unless $keytab->{default_keynum};
+    return $keytab->{default_keynum};
+}
+
+my $mac_secret = sub {
+    my %args = @_;
+    return sha256(&$get_secret(@_));    ## nocritic
+};
+
+=head2 $ self->encrypt_data(data => $data)
+
+Serialize I<$data> to JSON, encrypt it, and encode as base64. Also compute HMAC
+code for the encrypted data. Returns hash reference with 'data' and 'mac'
+elements.
+
+Args include
+
+=over
+
+=item data
+
+Data structure reference to be encrypted
+
+=item cypher
+
+Cypher to use (default: Rijndael)
+
+=cut
+
+sub encrypt_data {
+    my ($self, %args) = @_;
+    croak "data argument is required and must be a reference" unless $args{data} and ref $args{data};
+    my $json_data = encode_json($args{data});
+    my $cypher = $args{cypher} || 'Rijndael';
+    # Crypt::CBC generates random 8 bytes salt that it uses to
+    # derive IV and encryption key from $args{secret}. It uses
+    # the same algorythm as OpenSSL, the output is identical to
+    # openssl enc -e -aes-256-cbc -k $args{secret} -salt
+    my $cbc = Crypt::CBC->new(
+        -key => &$get_secret(
+            keyname => $self->keyname,
+            keynum  => $self->keynum,
+        ),
+        -cipher => $cypher,
+        -salt   => 1,
+    );
+    my $data = encode_base64($cbc->encrypt($json_data), '');
+    my $mac = hmac_sha256_base64(
+        $data,
+        &$mac_secret(
+            keyname => $self->keyname,
+            keynum  => $self->keynum
+        ));
+    return {
+        data => $self->keynum . '*' . $data,
+        mac  => $mac,
+    };
+} 
+
+=head2 $self->decrypt_data(data => $data, mac => $mac)
+
+Decrypt data encrypted using I<encrypt_data>. First checks HMAC code for data.
+If data was not tampered, decrypts it and decodes from JSON. Returns data, or
+undef if decryption failed.
+
+=cut
+
+sub decrypt_data {
+    my ($self, %args) = @_;
+    croak "method requires data and mac arguments" unless $args{data} and $args{mac};
+    # if the data was tampered do not try to decrypt it
+
+    my ($keynum, $cyphertext) = split /\*/, $args{data}, 2;
+
+    if (!$cyphertext) {
+        $cyphertext = $keynum;
+        $keynum     = 'none';
+    }
+    my $secret = &$get_secret(
+        keynum  => $keynum,
+        keyname => $self->keyname
+    );
+    return unless ($cyphertext and $secret);
+    my $msg_mac = hmac_sha256_base64(
+        $cyphertext,
+        &$mac_secret(
+            keynum  => $keynum,
+            keyname => $self->keyname,
+        ));
+    return unless String::Compare::ConstantTime::equals($msg_mac, $args{mac});
+
+    my $cbc = Crypt::CBC->new(
+        -key => &$get_secret(
+            keynum  => $keynum,
+            keyname => $self->keyname
+        ),
+        -cipher => 'Rijndael',
+        -salt   => 1,
+    );
+    my $decrypted = $cbc->decrypt(decode_base64($cyphertext));
+    warn "Unable to decrypt $args{data} with keynum $keynum and keyname " . $self->keyname unless defined $decrypted;
+    my $data = decode_json($decrypted);
+    return $data;
+}
+
+=head2 $self->encrypt_payload(data  => $data)
+
+Encrypts data using I<encrypt_data> and returns result as a string including 
+both cyphertext and hmac in base-64 format.  This can work on arbitrary data
+structures, scalars, and references provided that the data can be serialized
+as an attribute on a JSON document.
+
+=cut
+
+sub _to_payload {
+    my ($data) = @_;
+    return {
+        crypt_json_payload => $data,
+        crypt_json_version => $VERSION,
+    };
+}
+
+sub _from_payload {
+    my ($payload) = @_;
+    return unless defined $payload;
+    return $payload->{crypt_json_payload} if exists $payload->{crypt_json_payload};
+    return $payload;
+}
+
+sub encrypt_payload {
+    my ($self, %args) = @_;
+    $args{data} = _to_payload($args{data});
+    my $enc = $self->encrypt_data(%args);
+    my $res = $enc->{data};
+    $res .= '.' . $enc->{mac};
+    return $res;
+}
+
+=head2 $self->decrypt_payload(value => $value)
+
+Accepts payload encrypted with I<encrypt_payload>, checks HMAC and decrypts
+the value. Returns decripted value or undef if check or decryption has failed.
+
+=cut
+
+sub decrypt_payload {
+    my ($self, %args) = @_;
+    return unless $args{value};    # nothing to decrypt
+    $args{value} =~ /^([A-Za-z0-9+\/*]+=*)\.([A-Za-z0-9+\/]+)$/ or return;
+    my ($data, $mac) = ($1, $2);
+    return _from_payload(
+        $self->decrypt_data(
+            data => $data,
+            mac  => $mac,
+        ));
+}
+
+1;
